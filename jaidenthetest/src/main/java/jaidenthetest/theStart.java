d8196e9ceb421578f0b567dfa376abc792b53215
cmacias <cmacias@10.10.17.0>
2021-07-12 11:38:37 -0600

    changes

---
+++ jaidenthetest/src/main/java/jaidenthetest/theStart.java
@@ -0,0 +1,15 @@
+package jaidenthetest;
+
+public class theStart {
+
+	public static void main(String[] args) {
+
+		
+		int intReturnadd;
+		calculater clsCalc = new calculater();
+		intReturnadd = clsCalc.add(4, 5);
+		System.out.println(intReturnadd);
+	}
+	
+
+}
